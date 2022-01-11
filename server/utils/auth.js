12a435a97792a575c1e22ca75c8c3551ceee68ac
Kurdiamina3 <kurdiamina3@gmail.com>
2022-01-11 15:24:27 -0500

    project set up

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,6 +5,11 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  signToken: function({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
   // function for our authenticated routes
   authMiddleware: function (req, res, next) {
     // allows token to be sent via  req.query or headers
@@ -27,13 +32,7 @@ module.exports = {
       console.log('Invalid token');
       return res.status(400).json({ message: 'invalid token!' });
     }
-
+return req;
     // send to next endpoint
-    next();
-  },
-  signToken: function ({ username, email, _id }) {
-    const payload = { username, email, _id };
-
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  },
+  }
 };
