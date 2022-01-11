69146885c1334744bf5a1618a98a66d0c860b53d
Kurdiamina3 <kurdiamina3@gmail.com>
2022-01-11 17:05:49 -0500

    typo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/search-book', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
